2240f1257e477af7f6f787c0468c337bfdf122eb
flachet <nicolas.flachet@cstb.fr>
2016-04-19 14:19:30 +0200

    add arguments in command line

--- DataModule/__main__.py
+++ DataModule/__main__.py
@@ -1,15 +1,39 @@
-import sys
+import sys, getopt
 import tcpclient
 
-def main(args=None):
+def main(argv=None):
     """The main routine."""
-    if args is None:
-        args = sys.argv[1:]
+
+    if argv is None:
+        argv = sys.argv[1:]
+
+    host = '10.9.10.183'
+    dbname = 'Hovsjo_test'
+    user = 'tournaire'
+    password = 'olivier'
+    port = '5432'
+    try:
+        opts, args = getopt.getopt(argv, "h:d:u:P:p", ["host=", "dbname=", "user=", "password", "port="])
+    except getopt.GetoptError:
+        print 'DataModule.py -h <host> -d <database name> -u <user> -P <password> -p <port>'
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt in ('-h', '--host'):
+            host = arg
+        elif opt in ('-d', '--dbname'):
+            dbname = arg
+        elif opt in ('-u', '--user'):
+            user = arg
+        elif opt in ('-P', '--password'):
+            password = arg
+        elif opt in ('-p', '--port'):
+            port = arg
 
     print("Starting dataModule.")
 
     #testClient = tcpclient.TcpClient("10.9.10.183", "Hovsjo_test", "tournaire", "olivier", "5432")
-    testClient = tcpclient.TcpClient("localhost", "ecodistrict", "postgres", "FF511Aeris", "5432")
+    #testClient = tcpclient.TcpClient("localhost", "ecodistrict", "postgres", "FF511Aeris", "5432")
+    testClient = tcpclient.TcpClient(host, dbname, user, password, port)
     raw_input('dataModule is running; press return to quit.')
 
 if __name__ == "__main__":
