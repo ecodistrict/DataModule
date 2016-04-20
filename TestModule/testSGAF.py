4dc33ce2c6e6df0922d70d90d73497be61221640
flachet <nicolas.flachet@cstb.fr>
2016-04-20 11:13:12 +0200

    refactor one missing warning

--- TestModule/testSGAF.py
+++ TestModule/testSGAF.py
@@ -1,4 +1,4 @@
-import DataModule.tcpclient as tcpclient
+import DataModule.tcpclient as tcp_client
 import json
 
 
@@ -7,7 +7,7 @@ def main(args=None):
     simulate get Data case
     """
 
-    test_client = tcpclient.TcpClient("10.9.10.183", "Hovsjo_test", "tournaire", "olivier", "5432")
+    test_client = tcp_client.TcpClient("10.9.10.183", "Hovsjo_test", "tournaire", "olivier", "5432")
     get_data_message = {
                         "method": "get_data",
                         "type": "request",
